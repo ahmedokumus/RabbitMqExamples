2312c824f5d29d23c9dfac5864acf7346fde48ba
Ahmed Hakan Okumus <ahmed.aho666@gmail.com>
2024-07-30 16:33:19 +0300

    Fanout Exchange Training

---
+++ RabbitMq.Fanout.Exchange.Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "fanout-exchange-example",
+    type: ExchangeType.Fanout);
+
+Console.Write("Kuyruk adını giriniz : ");
+string? queueName = Console.ReadLine();
+
+//Static
+channel.QueueDeclare(
+    queue: queueName,
+    exclusive: false);
+
+channel.QueueBind(
+    queue: queueName,
+    exchange: "fanout-exchange-example",
+    routingKey: string.Empty);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (_, eventArgs) =>
+{
+    string message = Encoding.UTF8.GetString(eventArgs.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ RabbitMq.Fanout.Exchange.Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "fanout-exchange-example",
+    type: ExchangeType.Fanout);
+
+while (true)
+{
+    Console.Write("Mesaj = ");
+    string? message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message!);
+    channel.BasicPublish(
+        exchange: "fanout-exchange-example",
+        routingKey: string.Empty,
+        body: byteMessage);
+}
+
+//Fanout exchange, bu exchange bind olmuş bütün kuyrukların hepsine isim gözetmeksizin
+//bu mesajı iletecekse routing key boş geçilir.
