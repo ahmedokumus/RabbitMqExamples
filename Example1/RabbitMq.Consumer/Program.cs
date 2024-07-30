56dd8bb6ef83d38d6c49bd22f38ae9ab3fd51657
Ahmed Hakan Okumus <ahmed.aho666@gmail.com>
2024-07-30 13:02:32 +0300

    Add project files.

---
+++ Example1/RabbitMq.Consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+//Bağlantı oluşturma 
+var factory = new ConnectionFactory();
+factory.HostName = "localhost";
+
+//Bağlantı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue oluşturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false);//Consumer'daki kuyruk publisher'daki kuyruk ile birebir aynı olmalıdır
+
+//Queue'dan mesaj okuma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", autoAck: false, consumer);
+consumer.Received += (sender, e) =>
+{
+    //Kuyruğa gelen mesajın işlendiği yerdir.
+    //e.Body : Kuyruktaki mesajın verisini bütünsel olarak getirecektir
+    //e.Body.Span veya e.Body.ToArray() : Kuyruktaki mesajın byte verisini getirecektir.
+    var message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ Example1/RabbitMq.Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+//Bağlantı oluşturma 
+var factory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+//Bağlantı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue Oluşturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Queue'ya mesaj gönderme
+////RabbitMq queue ye atacağı mesajları byte türünden kabul etmektedir. Haliyle mesajları bizim byte'a dönüştürmemiz gerekecektir.
+
+//byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+//channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(250);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i}");
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+}
+
+Console.Read();
