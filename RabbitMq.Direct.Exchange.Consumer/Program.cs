875de2b10ec8568798e6eb4f7aa2fb14c26dada9
Ahmed Hakan Okumus <ahmed.aho666@gmail.com>
2024-07-30 15:41:01 +0300

    Direct Exchange Training

---
+++ RabbitMq.Direct.Exchange.Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(
+    queue: queueName,
+    exchange: "direct-exchange-example",
+    routingKey: "direct-exchange-example");
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
+
+// 1. Adım : Publisher'da ki exchange ile birebir aynı isim ve
+// type 'a sahip bir exchange tanımlanmalıdır!
+
+// 2. Adım : Publisher tarafından routing key 'de bulunan değerdeki
+// kuyruğa gönderilen mesajları, kendi oluşturduğumuz kuyruğa yönlendirerek
+// tüketmemiz gerekmektedir. Bunun için öncelikle bir kuyruk oluşturulmalıdır !
---
+++ RabbitMq.Direct.Exchange.Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+while (true)
+{
+    Console.Write("Mesaj : ");
+    string? message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message!);
+
+    channel.BasicPublish(
+        exchange: "direct-exchange-example",
+        routingKey: "direct-exchange-example",
+        body: byteMessage);
+}
