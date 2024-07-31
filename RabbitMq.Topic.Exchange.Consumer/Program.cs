77e33633c58578bca1c308fb9ad717f13d51de7e
Ahmed Hakan Okumus <ahmed.aho666@gmail.com>
2024-07-31 22:36:53 +0300

    Topic Exchange Training

---
+++ RabbitMq.Topic.Exchange.Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string exchange = "topic-exchange-example";
+
+channel.ExchangeDeclare(
+    exchange: exchange,
+    type: ExchangeType.Topic);
+
+Console.Write("Topic belirtiniz : ");
+string topic = Console.ReadLine()!;
+
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(
+    exchange: exchange,
+    queue: queueName,
+    routingKey: topic);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ RabbitMq.Topic.Exchange.Publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string exchange = "topic-exchange-example";
+
+channel.ExchangeDeclare(
+    exchange: exchange,
+    type: ExchangeType.Topic);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(250);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i}");
+    Console.Write("Topic belirtiniz : ");
+    string topic = Console.ReadLine()!;
+
+    channel.BasicPublish(
+        exchange: exchange,
+        routingKey: topic,
+        body: message);
+}
+
+Console.Read();
