3ffe8eae4340894aef681573f31786f5fc5a2f59
Ahmed Hakan Okumus <ahmed.aho666@gmail.com>
2024-08-03 17:27:04 +0300

    Header Exchange Training

---
+++ RabbitMq.Header.Exchange.Consumer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string exchange = "header-exchange-example";
+
+channel.ExchangeDeclare(
+    exchange: exchange,
+    type: ExchangeType.Headers);
+
+Console.Write("Lütfen header value giriniz : ");
+string value = Console.ReadLine()!;
+
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(
+    queue: queueName,
+    exchange: exchange,
+    routingKey: string.Empty,
+    new Dictionary<string, object>
+    {
+        ["no"] = value,
+    });
+
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ RabbitMq.Header.Exchange.Publisher/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string exchange = "header-exchange-example";
+
+channel.ExchangeDeclare(
+    exchange: exchange,
+    type: ExchangeType.Headers);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i}");
+    Console.Write("Lütfen header value giriniz : ");
+    string value = Console.ReadLine()!;
+
+    IBasicProperties basicProperties = channel.CreateBasicProperties();
+
+    basicProperties.Headers = new Dictionary<string, object>
+    {
+        //["x-match"] = "any", "all" //x-match any ve all olmak üzere 2 değer alır.
+        ["no"] = value
+    };
+
+    channel.BasicPublish(
+        exchange: exchange,
+        routingKey: string.Empty,
+        body: message,
+        basicProperties: basicProperties);
+}
